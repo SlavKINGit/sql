28859a3e6aa139b23376fac9e05baee1e326a8bc
SlavKINGit <ivankinw@inbox.ru>
2022-12-09 14:05:25 +0300

    revision 1

--- HW.sql
+++ HW.sql
@@ -1,42 +1,45 @@
 CREATE TABLE IF NOT EXISTS genres(
-	genres_id int PRIMARY KEY,
-	genres_name TEXT NOT NULL UNIQUE
+	genre_id serial PRIMARY KEY,
+	genre_name TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS performers(
-	performers_id int PRIMARY KEY,
-	performers_name TEXT NOT NULL
+	performer_id serial PRIMARY KEY,
+	performer_name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS genres_performers(
-	genres_id int REFERENCES genres(genres_id),
-	performers_id int REFERENCES performers(performers_id),
-	CONSTRAINT pk_genres_performers PRIMARY KEY (genres_id, performers_id)
+	genre_id int REFERENCES genres(genre_id),
+	performer_id int REFERENCES performers(performer_id),
+	CONSTRAINT pk_genres_performers PRIMARY KEY (genre_id, performer_id)
 );
 
 CREATE TABLE IF NOT EXISTS albums(
-	albums_id int PRIMARY KEY,
-	albums_name TEXT NOT NULL UNIQUE,
+	album_id serial PRIMARY KEY,
+	album_name TEXT NOT NULL UNIQUE,
 	date_of_release date NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS performers_albums(
-	performers_id int REFERENCES performers(performers_id),
-	albums_id int REFERENCES albums(albums_id),
-	CONSTRAINT pk_performers_albums PRIMARY KEY (performers_id, albums_id)
+	performer_id int REFERENCES performers(performer_id),
+	album_id int REFERENCES albums(album_id),
+	CONSTRAINT pk_performers_albums PRIMARY KEY (performer_id, album_id)
 );
 
 CREATE TABLE IF NOT EXISTS tracks(
-	tracks_id int PRIMARY KEY,
-	tracks_name TEXT NOT NULL UNIQUE,
-	duration time(2) NOT NULL,
-	albums_id int REFERENCES albums(albums_id)
+	track_id serial PRIMARY KEY,
+	track_name TEXT NOT NULL UNIQUE,
+	duration int NOT NULL,
+	album_id int REFERENCES albums(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS collection(
+	collection_id serial PRIMARY KEY,
 	collection_name TEXT NOT NULL UNIQUE,
-	date_of_release date NOT NULL,
-	albums_id int NOT NULL REFERENCES albums(albums_id),
-	tracks_id int NOT NULL REFERENCES tracks(tracks_id),
-	CONSTRAINT pk_collection PRIMARY KEY (albums_id, tracks_id)
+	date_of_release date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS track_collection(
+	track_id int NOT NULL REFERENCES tracks(track_id),
+	collection_id int NOT NULL REFERENCES collection(collection_id)
 )
